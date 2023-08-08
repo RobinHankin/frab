e48c334158680af779550ed7766c878d166a7686
Robin Hankin <hankin.robin@gmail.com>
2023-08-08 21:08:30 +1200

    bugfix

--- R/sparsetable.R
+++ R/sparsetable.R
@@ -87,7 +87,7 @@ setMethod("show", "sparsetable", function(object){print_sparsetable_matrixform(o
     if(is.sparsetable(x)){
         return(x)
     } else if(is.frab(x)){
-        return(sparsetable(cbind(names(y)),values(y)))
+        return(sparsetable(cbind(names(x)),values(x)))
     } else if(is.list(x)){
         return(sparsetable(x$index,x$value))
     } else if(is.table(x)){
